228a2a0c6967cd82a4114801501aa0642697d0cc
Nicolas Carcamo <nicolas.carcamo.e@usach.cl>
2023-11-06 07:56:51 -0300

    Cambios

--- topEduEstudiantes/src/main/java/com/example/topEduEstudiantes/entity/ColegioEntity.java
+++ topEduEstudiantes/src/main/java/com/example/topEduEstudiantes/entity/ColegioEntity.java
@@ -1,7 +1,7 @@
 package com.example.topEduEstudiantes.entity;
 
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
