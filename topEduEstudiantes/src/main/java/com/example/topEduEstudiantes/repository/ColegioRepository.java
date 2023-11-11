1188a7f86f2a7c48a1e0e6fa2417fb3f9dabe70f
Nicolas Carcamo <nicolas.carcamo.e@usach.cl>
2023-11-11 00:33:21 -0300

    problema k8s

--- topEduEstudiantes/src/main/java/com/example/topEduEstudiantes/repository/ColegioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.topEduEstudiantes.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.topEduEstudiantes.entity.ColegioEntity;
-
-public interface ColegioRepository extends CrudRepository<ColegioEntity, Long> {
-}
--- topEduEstudiantes/src/main/java/com/example/topEduEstudiantes/repository/TipoColegioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.topEduEstudiantes.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.topEduEstudiantes.entity.TipoColegioEntity;
-
-public interface TipoColegioRepository extends CrudRepository<TipoColegioEntity, Long> {
-}
